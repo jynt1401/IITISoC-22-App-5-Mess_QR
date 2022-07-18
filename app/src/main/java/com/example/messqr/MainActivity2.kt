240a4cd66967b120d726f77547f2f9f2f9be507f
Mihir <mihirkaushik03@gmail.com>
2022-07-18 16:45:03 +0530

    second commit

--- app/src/main/java/com/example/messqr/MainActivity2.kt
+++ app/src/main/java/com/example/messqr/MainActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.messqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val signUp:TextView=findViewById(R.id.signUp)
+        signUp.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
