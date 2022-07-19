19d6ee61eef7546144014dd88ac6b6674bec4e87
Ishaan Mittal <ishaanmittal123@gmail.com>
2022-07-19 18:36:04 +0530

    commit 5

--- app/src/main/java/com/example/messqr/MainActivity5.kt
+++ app/src/main/java/com/example/messqr/MainActivity5.kt
@@ -1,11 +1,37 @@
 package com.example.messqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main5)
+
+        //handle click announcement Image button
+        val announceImageButtonButton: ImageButton =  findViewById(R.id.announce_Ibutton)
+        announceImageButtonButton.setOnClickListener {
+            Intent(this,MainActivity9::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        //handle click scanner Image button
+        val scanImageButton: ImageButton =  findViewById(R.id.scan_Ibutton)
+        scanImageButton.setOnClickListener {
+            Intent(this,MainActivity4::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        //handle click history Image button
+        val historyImageButton: ImageButton =  findViewById(R.id.history_Ibutton)
+        historyImageButton.setOnClickListener {
+            Intent(this,MainActivity10::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
