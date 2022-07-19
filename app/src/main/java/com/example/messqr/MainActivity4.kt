3963857136b64fafcd9c0245d024c1a227a4ddea
Ishaan Mittal <ishaanmittal123@gmail.com>
2022-07-19 18:36:04 +0530

    commit 5

--- app/src/main/java/com/example/messqr/MainActivity4.kt
+++ app/src/main/java/com/example/messqr/MainActivity4.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 
+
+
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,18 +16,41 @@ class MainActivity4 : AppCompatActivity() {
         //handle click special meal button
         val button4:Button=findViewById(R.id.button4)
         button4.setOnClickListener {
-            val intent=Intent(this,MainActivity8::class.java)
-            startActivity(intent)
+            Intent(this,MainActivity8::class.java).also{
+                startActivity(it)
+            }
         }
 
+        //handle click announcement Image button
+        val announceImageButtonButton:ImageButton =  findViewById(R.id.announce_Ibutton)
+        announceImageButtonButton.setOnClickListener {
+            Intent(this,MainActivity9::class.java).also{
+                startActivity(it)
+            }
+        }
 
+        //handle click payment Image button
+        val paymentImageButton:ImageButton =  findViewById(R.id.payment_Ibutton)
+        paymentImageButton.setOnClickListener {
+            Intent(this,MainActivity5::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        //handle click history Image button
+        val historyImageButton:ImageButton =  findViewById(R.id.history_Ibutton)
+        historyImageButton.setOnClickListener {
+            Intent(this,MainActivity10::class.java).also{
+                startActivity(it)
+            }
+        }
 
         //this is only for reference that how it will work
-        val button6:Button=findViewById(R.id.button6)
-        button6.setOnClickListener {
-            val intent2=Intent(this, MainActivity10::class.java)
-            startActivity(intent2)
-        }
+//        val button6:Button=findViewById(R.id.button6)
+//        button6.setOnClickListener {
+//            val intent2=Intent(this, MainActivity10::class.java)
+//            startActivity(intent2)
+//        }
 
         //rupee navigate button
 //        val rupeeBtn:ImageButton=findViewById(R.id.imageButton9)
--- app/src/main/java/com/example/messqr/MainActivity8.kt
+++ app/src/main/java/com/example/messqr/MainActivity8.kt
@@ -1,11 +1,37 @@
 package com.example.messqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity8 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_8main)
+
+        //handle click announcement Image button
+        val announceImageButtonButton: ImageButton =  findViewById(R.id.announce_Ibutton)
+        announceImageButtonButton.setOnClickListener {
+            Intent(this,MainActivity9::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        //handle click payment Image button
+        val paymentImageButton: ImageButton =  findViewById(R.id.payment_Ibutton)
+        paymentImageButton.setOnClickListener {
+            Intent(this,MainActivity5::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        //handle click history Image button
+        val historyImageButton: ImageButton =  findViewById(R.id.history_Ibutton)
+        historyImageButton.setOnClickListener {
+            Intent(this,MainActivity10::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
