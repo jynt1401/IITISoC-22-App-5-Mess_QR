69b9d9383bcdec4fe7b0f5301b84a1d1cea4419a
Mihir <mihirkaushik03@gmail.com>
2022-07-19 11:35:18 +0530

    fourth commit

--- app/src/main/java/com/example/messqr/MainActivity4.kt
+++ app/src/main/java/com/example/messqr/MainActivity4.kt
@@ -1,11 +1,38 @@
 package com.example.messqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        //handle click special meal button
+        val button4:Button=findViewById(R.id.button4)
+        button4.setOnClickListener {
+            val intent=Intent(this,MainActivity8::class.java)
+            startActivity(intent)
+        }
+
+
+
+        //this is only for reference that how it will work
+        val button6:Button=findViewById(R.id.button6)
+        button6.setOnClickListener {
+            val intent2=Intent(this, MainActivity10::class.java)
+            startActivity(intent2)
+        }
+
+        //rupee navigate button
+//        val rupeeBtn:ImageButton=findViewById(R.id.imageButton9)
+//        rupeeBtn.setOnClickListener {
+//            val intent3=Intent(this, MainActivity5::class.java)
+//            startActivity(intent3)
+//        }
+
     }
 }
